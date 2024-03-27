944b4045e07e08c31d866bc8ef490fb0eb2729be
mutiarahmatun <mutia.husnarahmatun@gmail.com>
2024-03-27 10:33:04 +0700

    add testcase 16 until 20

--- src/test/java/com/seleniumadvanced/tests/TestCase16.java
+++ src/test/java/com/seleniumadvanced/tests/TestCase16.java
@@ -1,4 +1,57 @@
 package com.seleniumadvanced.tests;
 
+import com.seleniumadvanced.pages.CartPage;
+import com.seleniumadvanced.pages.HomePage;
+import com.seleniumadvanced.pages.LoggedHomePage;
+import com.seleniumadvanced.utils.JSONReader;
+import io.qameta.allure.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import org.json.simple.parser.ParseException;
+
+@Epic("Regression Tests")
+@Feature("Place Order")
 public class TestCase16 extends TestBasic {
+    @Test(description = "Test Case 16: Place Order: Login before Checkout")
+    @Severity(SeverityLevel.CRITICAL)
+    @Story("Place Order: Login before Checkout")
+    @Description("""
+            1. Launch browser
+            2. Navigate to url 'http://automationexercise.com'
+            3. Verify that home page is visible successfully
+            4. Click 'Signup / Login' button
+            5. Fill email, password and click 'Login' button
+            6. Verify 'Logged in as username' at top
+            7. Add products to cart
+            8. Click 'Cart' button
+            9. Verify that cart page is displayed
+            10. Click Proceed To Checkout
+            11. Verify Address Details and Review Your Order
+            12. Enter description in comment text area and click 'Place Order'
+            13. Enter payment details: Name on Card, Card Number, CVC, Expiration date
+            14. Click 'Pay and Confirm Order' button
+            15. Verify success message 'Your order has been placed successfully!'
+            """)
+    public void placeOrderLoginBeforeCheckout() throws IOException, ParseException {
+        TestCase1.verifyThatHomePageIsVisibleSuccessfully();
+        new HomePage(getDriver())
+                .signupLoginClick()
+                .fillCorrectLogin(JSONReader.existingUser("email"), JSONReader.existingUser("password"));
+        verifyLoggedInAsUsernameAtTop();
+        TestCase14.verifyThatCartPageIsDisplayed();
+        new CartPage(getDriver()).proceedToCheckoutButtonClick();
+        TestCase14.verifyAddressDetailsAndReviewYourOrder();
+        TestCase14.verifySuccessMessageCongratulationsYourOrderHasBeenConfirmed();
+    }
+
+    @Step("Verify 'Logged in as username' at top")
+    private void verifyLoggedInAsUsernameAtTop() throws IOException, ParseException {
+        String username = new LoggedHomePage(getDriver())
+                .getUsername()
+                .getText();
+        Assert.assertEquals(username, JSONReader.existingUser("name"), "Verify 'Logged in as username' at top");
+    }
+
 }
