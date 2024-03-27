5e4e90141df44c3e80b094f7c684b925f08e44ec
mutiarahmatun <mutia.husnarahmatun@gmail.com>
2024-03-27 10:06:41 +0700

    initial commit project

---
+++ src/test/java/com/seleniumadvanced/utils/Util.java
@@ -0,0 +1,11 @@
+package com.seleniumadvanced.utils;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Util {
+
+    public static String generateCurrentDateAndTime() {
+        return new SimpleDateFormat("ddMMyyyyHHmmssSSS").format(new Date());
+    }
+}
